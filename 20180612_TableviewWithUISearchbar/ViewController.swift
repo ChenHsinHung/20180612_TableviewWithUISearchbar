4c583793d72043e40015fe89ff218137b669ebfd
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2018-06-12 22:41:31 +0800

    Initial Commit

---
+++ 20180612_TableviewWithUISearchbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180612_TableviewWithUISearchbar
+//
+//  Created by chen hsin hung on 2018/6/12.
+//  Copyright © 2018年 chen hsin hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
